8e539d26463ad534ab9ac7420c94114299717b6d
santhoshkumar <sanss2620818@gmail.com>
2025-07-21 01:22:28 +0530

    update javafm file

--- hellofm.java
+++ hellofm.java
@@ -1,5 +1,6 @@
 class hellofm{
     public static void main(String []args){
         System.out.println("Hii Helloworld");
+        System.out.println("Hii Buddy welcome this is intersitng");
     }
 }
